33c764603b5c5ca61cce688293466106ee88eb57
Jacob Knittle <jacobdknittle@gmail.com>
2026-01-06 10:04:06 -0800

    Create Book constructor function

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
