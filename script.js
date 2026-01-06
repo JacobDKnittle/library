4a65457dd6f2a5e43745255ee3a18b3c59373fca
Jacob Knittle <jacobdknittle@gmail.com>
2026-01-06 10:18:58 -0800

    add book to library function

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.id = id;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, read) {
   // take params, create a book then store it in the array
+    const id = crypto.randomUUID()
+   myLibrary.push(new Book(title, author, pages, read, id))
 }
+
+addBookToLibrary('test', 'guy', 230, 'yes');
